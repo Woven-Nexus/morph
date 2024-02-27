04e0bf432477992e56be2ea320deb211d030fbc7
KristofferRoenLie <KristofferRoenLie@gmail.com>
2024-02-28 00:14:20 +0100

    Started creating a field config

--- packages/forge/src/features/ide/file-details.cmp.ts
+++ packages/forge/src/features/ide/file-details.cmp.ts
@@ -1,8 +1,11 @@
 import { AegisElement, customElement } from '@roenlie/lit-aegis';
-import { html } from 'lit';
+import { sharedStyles } from '@roenlie/mimic-lit/styles';
+import { css, html, nothing } from 'lit';
 import { property, state } from 'lit/decorators.js';
+import { live } from 'lit/directives/live.js';
 import { map } from 'lit/directives/map.js';
 
+import { CreateFields } from '../field-config/field-config.js';
 import { ForgeFile, ForgeFileDB } from '../filesystem/forge-file.js';
 import { MimicDB } from '../filesystem/mimic-db.js';
 
@@ -12,12 +15,35 @@ export class FileDetailsCmp extends AegisElement {
 
 	@property() public activeId?: string;
 	@state() protected file?: ForgeFile;
+	protected config: FieldType[] = [];
 
-	protected override updated(props: Map<PropertyKey, unknown>): void {
-		super.updated(props);
+	protected override willUpdate(props: Map<PropertyKey, unknown>): void {
+		super.willUpdate(props);
 
 		if (props.has('activeId'))
 			this.getFile();
+
+		if (props.has('file')) {
+			this.config = CreateFields.input({
+				label: 'name',
+				value: this.file?.name,
+			}).select({
+				label:   'type',
+				value:   this.file?.type,
+				options: [
+					{
+						key:         'script',
+						value:       'script',
+						description: 'script',
+					},
+					{
+						key:         'component',
+						value:       'component',
+						description: 'component',
+					},
+				],
+			}).build();
+		}
 	}
 
 	protected async getFile() {
@@ -31,21 +57,72 @@ export class FileDetailsCmp extends AegisElement {
 	}
 
 	protected override render(): unknown {
-		const keyValues = [
-			[ 'name', this.file?.name ],
-			[ 'type', this.file?.type ],
-		];
+		const setValue = (event: Event, field: FieldType) => {
+			const target = event.currentTarget as unknown as {value: any};
+			field.value = target.value;
 
+			console.log(this.config);
+		};
 
 		return html`
-		${ map(keyValues, ([ key, value ]) => html`
-		<div>
-			<span>${ key }:</span>
-			<span>${ value }</span>
-		</div>
-		`) }
+		<form>
+		${ map(this.config, (field) => {
+			if (CreateFields.isInput(field)) {
+				return html`
+				<label for=${ field.label }>
+					<span>${ field.label }</span>
+					<input
+						id=${ field.label }
+						.value=${ live(field.value) }
+						@change=${ (ev: Event) => setValue(ev, field) }
+					></input>
+				</label>
+				`;
+			}
+			if (CreateFields.isSelect(field)) {
+				return html`
+				<label for=${ field.label }>
+					${ field.label }
+					<select
+						id=${ field.label }
+						.value=${ live(field.value) }
+						@change=${ (ev: Event) => setValue(ev, field) }
+					>
+						${ map(field.options, option => html`
+						<option .value=${ option.value }>
+							${ option.description ?? option.key ?? option.value }
+						</option>
+						`) }
+					</select>
+				</label>
+				`;
+			}
+
+			return nothing;
+		}) }
+		</form>
 		`;
 	}
 
+	public static override styles = [
+		sharedStyles,
+		css`
+		:host {
+			display: block;
+			padding: 8px;
+		}
+		form {
+			display: grid;
+			grid-template-columns: max-content 1fr;
+			gap: 8px;
+		}
+		label {
+			display: grid;
+			grid-template-columns: subgrid;
+			grid-column: 1/3;
+			gap: 8px;
+		}
+		`,
+	];
 
 }
