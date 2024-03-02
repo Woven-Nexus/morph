62d2855e87d19c6c0b945f6bacaa54df43217d36
KristofferRoenLie <KristofferRoenLie@gmail.com>
2024-03-02 03:39:55 +0100

    doing some fun work testing out if I can make a html tag template function that sits before Lit HTML and allows for a JSX type of componeny syntax.

--- packages/forge/src/pages/settings/settings.cmp.ts
+++ packages/forge/src/pages/settings/settings.cmp.ts
@@ -1,6 +1,23 @@
 import { AegisElement, customElement } from '@roenlie/lit-aegis';
 import { html } from 'lit';
 
+import { dirHtml } from '../../features/forge-element/dir-element.js';
+
+
+//const result = dirHtml`
+//	<div>
+//		${ 'something1' }
+//		<Hello label=${ 'hello' }>
+//		${ '<span></span>' }
+//		</Hello>
+//	</div>
+//	${ 'something2' }
+//	`;
+//const result = dirHtml`<div><Hello></Hello></div>`;
+const result = dirHtml`<div><Hello>${ 'jaman' }</Hello></div>`;
+
+console.log(result);
+
 
 @customElement('m-settings-page', true)
 export class EditorPageCmp extends AegisElement {
@@ -9,7 +26,7 @@ export class EditorPageCmp extends AegisElement {
 
 	protected override render(): unknown {
 		return html`
-		SETTINGS
+		${ result }
 		`;
 	}
 
