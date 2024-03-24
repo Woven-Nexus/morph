fe17f0009f48361b810e17d637aa3afa30948238
KristofferRoenLie <KristofferRoenLie@gmail.com>
2024-03-24 17:38:16 +0100

    editor output worker imports now point to their relative location, making it possible to copy paste and serve the editor from any directory.

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,10 +1,11 @@
 import { libConfig } from '@roenlie/package-toolbox/vite-utils';
-import { defineConfig } from 'vite';
+import { defineConfig, type Plugin } from 'vite';
 
 
 export default defineConfig(libConfig({
 	build: {
 		emptyOutDir:   true,
+		outDir:        './dist/monaco',
 		rollupOptions: {
 			external: undefined,
 			output:   {
@@ -13,6 +14,31 @@ export default defineConfig(libConfig({
 			},
 		},
 	},
+	plugins: [
+		// This plugin makes the vitejs generated web worker wrappers
+		// point to the relative /assets location instead of the root url.
+		(() => {
+			return {
+				name:    'relative-worker-wrapper',
+				enforce: 'post',
+				transform(code, id) {
+					if (!id.includes('.worker'))
+						return;
+
+					const index = code.indexOf('"__VITE_WORKER_ASSET');
+					if (index < 0)
+						return;
+
+					const chars = [ ...code ];
+					chars.splice(
+						index, 0, 'import.meta.url.split("/").slice(0, -1).join("/") + ',
+					);
+
+					return chars.join('');
+				},
+			} as Plugin;
+		})(),
+	],
 	worker: {
 		rollupOptions: {
 			output: {
